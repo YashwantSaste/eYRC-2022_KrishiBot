6fa2f57ba6a6633d32cc9c43e0f5ff053f7ef5dc
YashwantSaste <97030432+YashwantSaste@users.noreply.github.com>
2022-10-03 16:26:23 +0530

    Commit

--- eyrc-2022_krishibot/scripts/task_0.py
+++ eyrc-2022_krishibot/scripts/task_0.py
@@ -83,7 +83,22 @@ def main():
 	Example call:
 	---
         main()
-	"""    
+	"""  
+    rospy.init_node('turtlesim', anonymous=True)
+    pub = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=10)
+    rate = rospy.Rate(10)
+    vel = Twist()
+    while not rospy.is_shutdown():
+        vel.linear.x = radius
+        vel.linear.y = 0
+        vel.linear.z = 0
+        vel.angular.x = 0
+        vel.angular.y = 0
+        vel.angular.z = 1
+        rospy.loginfo("Radius = %f",
+                      radius)
+        pub.publish(vel)
+        rate.sleep()
 
 ######### YOU ARE NOT ALLOWED TO MAKE CHANGES TO THIS PART #########    
 if __name__ == "__main__":
